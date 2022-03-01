ea3a302e049f13ee63d5a01502f8337a217a8478
Daniel Mössner <github@tortuga-webdesign.de>
2022-03-01 17:34:32 +0100

    INITIAL

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ git pull
 echo REQS
 tmp/venv/bin/pip install -r requirements.txt
 echo MIGRATE
+tmp/venv/bin/python manage.py collectstatic --no-input
 tmp/venv/bin/python manage.py migrate
 echo PERMISSIONS
 ./permissions.sh
