35087790a47e5a32d2aeffad6399e4df27f9cc32
Daniel Mössner <github@tortuga-webdesign.de>
2022-03-01 17:22:29 +0100

    INITIAL

--- permissions.sh
+++ permissions.sh
@@ -1,8 +1,8 @@
 chown -R root:www-data ./
 chmod -R 750 ./
-find ./ -type f -print0|xargs -0 chmod 740
+find ./ -type f -exec chmod 740 {} \;
 chmod -R 770 ./tmp/media
-find ./tmp/media -type f -print0|xargs -0 chmod 760
+find ./tmp/media -type f -exec chmod 760 {} \;
 chmod 770 ./tmp/logs
 chmod -R 760 ./tmp/logs/*
 chmod 770 ./tmp
