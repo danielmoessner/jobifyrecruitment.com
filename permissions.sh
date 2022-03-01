ee8b5480c02dc734a9049eab362da67d244950d8
Daniel Mössner <github@tortuga-webdesign.de>
2022-03-01 17:36:21 +0100

    INITIAL

--- permissions.sh
+++ permissions.sh
@@ -3,6 +3,5 @@ chmod -R 750 ./
 find ./ -type f -exec chmod 740 {} \;
 chmod -R 770 ./tmp/media
 find ./tmp/media -type f -exec chmod 760 {} \;
-chmod 770 ./tmp/logs
-chmod -R 760 ./tmp/logs/*
+chmod 760 ./tmp/django.log
 chmod 770 ./tmp
