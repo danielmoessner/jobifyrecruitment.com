723d6f25d15030f2195916ea8e173be15c967990
Daniel Mössner <github@tortuga-webdesign.de>
2022-03-01 17:23:35 +0100

    INITIAL

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,12 @@
+echo CODE
 git reset --hard HEAD
 git pull
+echo REQS
 tmp/venv/bin/pip install -r requirements.txt
+echo MIGRATE
 tmp/venv/bin/python manage.py migrate
+echo PERMISSIONS
 ./permissions.sh
+echo SERVER
 systemctl restart apache2
+echo DEPLOYED
