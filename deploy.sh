29015167054e89606a8dca5f0c7fa8cc34074853
Daniel Mössner <github@tortuga-webdesign.de>
2022-03-04 18:51:38 +0100

    add compilemessages to build

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,8 @@ tmp/venv/bin/pip install -r requirements.txt
 echo DJANGO
 tmp/venv/bin/python manage.py collectstatic --no-input
 tmp/venv/bin/python manage.py migrate
+echo TRANSLATIONS
+tmp/venv/bin/django-admin compilemessages
 echo PERMISSIONS
 ./permissions.sh
 echo SERVER
