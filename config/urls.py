0dbf3e351c7cb6655873bcbf6a49254e77586d40
Daniel Mössner <github@tortuga-webdesign.de>
2022-06-03 23:45:42 +0200

    remove sitemap

--- config/urls.py
+++ config/urls.py
@@ -11,7 +11,7 @@ sitemaps = {}
 
 urlpatterns = [
     # sitemap
-    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    # path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
     # tinymce
     path('tinymce/', include('tinymce.urls')),
     # robots file
